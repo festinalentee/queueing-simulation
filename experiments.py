6b52530b824a33a5f6e066dec576fd19fd984cd2
festinalentee <karolina.bienkowska550@gmail.com>
2023-06-05 21:01:12 +0200

    new commit

--- experiments.py
+++ experiments.py
@@ -18,6 +18,8 @@ def run_experiment(params):
     data = {'avg_time_spent': [], 'max_time_spent': [], 'server_utilization': [], 'avg_queue_size': [], 'server_speed_to_load': []}
     
     for i in range(0, params['num_experiments']):
+        if i % 100 == 0:
+            print(i)
         servers = initialize_servers(params['service_times'][i], params['num_servers'])
         simulation = Simulation(params['interarrival_times'][i], servers, params['num_servers'], params['num_customers'])
         avg_time_spent, max_time_spent, server_utilization, avg_queue_size = simulation.run(params['scenario'], params['d_choices'], params['servers_speed'])
@@ -43,9 +45,9 @@ s1_uss, s1_uss_d2, s1_uss_d3, s1_uss_d5 = {}, {}, {}, {}
 s2_uss, s2_uss_d2, s2_uss_d3, s2_uss_d5, s2_nuss_d2, s2_nuss_d3, s2_nuss_d5 = {}, {}, {}, {}, {}, {}, {}
 
 service_rate = 1.0
-num_servers = 5
-num_customers = 10
-num_experiments = 5
+num_servers = 10
+num_customers = 10000
+num_experiments = 1000
 
 for arrival_rate in [0.5, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]:
     print(arrival_rate)
@@ -58,11 +60,9 @@ for arrival_rate in [0.5, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]:
         interarrival_times.append(get_interarrival_time(arrival_rate))
         same_service_times.append([service_time for _ in range(num_servers)])
         different_service_times.append([get_service_time(service_rate) for _ in range(num_servers)])
-    
-    print(interarrival_times)
-    print(same_service_times)
 
-    # scenario_1
+    print('SCENARIO_1')
+    print('s1_uss')
     combined_data.setdefault('s1_uss', {})[arrival_rate] = run_experiment(
         {'num_servers': num_servers, 
         'num_customers': num_customers,
@@ -73,6 +73,7 @@ for arrival_rate in [0.5, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]:
         'd_choices': 0, 
         'servers_speed': 'same'})
     
+    print('s1_uss_d2')
     combined_data.setdefault('s1_uss_d2', {})[arrival_rate] = run_experiment(
         {'num_servers': num_servers, 
         'num_customers': num_customers,
@@ -83,6 +84,7 @@ for arrival_rate in [0.5, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]:
         'd_choices': 2, 
         'servers_speed': 'same'})
 
+    print('s1_uss_d3')
     combined_data.setdefault('s1_uss_d3', {})[arrival_rate] = run_experiment(
         {'num_servers': num_servers, 
         'num_customers': num_customers,
@@ -93,6 +95,7 @@ for arrival_rate in [0.5, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]:
         'd_choices': 3, 
         'servers_speed': 'same'})
 
+    print('s1_uss_d5')
     combined_data.setdefault('s1_uss_d5', {})[arrival_rate] = run_experiment(
         {'num_servers': num_servers, 
         'num_customers': num_customers,
@@ -103,7 +106,8 @@ for arrival_rate in [0.5, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]:
         'd_choices': 5, 
         'servers_speed': 'same'})
     
-    # scenario_2
+    print('SCENARIO_2')
+    print('s2_uss')
     combined_data.setdefault('s2_uss', {})[arrival_rate] = run_experiment(
         {'num_servers': num_servers, 
         'num_customers': num_customers,
@@ -114,6 +118,7 @@ for arrival_rate in [0.5, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]:
         'd_choices': 0, 
         'servers_speed': 'different'})
     
+    print('s2_uss_d2')
     combined_data.setdefault('s2_uss_d2', {})[arrival_rate] = run_experiment(
         {'num_servers': num_servers, 
         'num_customers': num_customers,
@@ -124,6 +129,7 @@ for arrival_rate in [0.5, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]:
         'd_choices': 2, 
         'servers_speed': 'different'})
 
+    print('s2_uss_d3')
     combined_data.setdefault('s2_uss_d3', {})[arrival_rate] = run_experiment(
         {'num_servers': num_servers, 
         'num_customers': num_customers,
@@ -134,6 +140,7 @@ for arrival_rate in [0.5, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]:
         'd_choices': 3, 
         'servers_speed': 'different'})
 
+    print('s2_uss_d5')
     combined_data.setdefault('s2_uss_d5', {})[arrival_rate] = run_experiment(
         {'num_servers': num_servers, 
         'num_customers': num_customers,
